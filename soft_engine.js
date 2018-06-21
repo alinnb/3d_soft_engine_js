181c7db631ccb58ba44fa32d2b63c9cb92e19082
alinnb <mailzyy@qq.com>
2018-06-21 20:40:40 +0800

    完成基础面的绘制；了解Bresenham算法画线

--- soft_engine.js
+++ soft_engine.js
@@ -15,9 +15,10 @@ define(['babylon'], function (BABYLON) {
 
 		SoftEngine.Camera = Camera;
 		var Mesh = (function () {
-			function Mesh(name, verticesCount) {
+			function Mesh(name, verticesCount, facesCount) {
 				this.name = name;
 				this.Vertices = new Array(verticesCount);
+				this.Faces = new Array(facesCount);
 				this.Rotation = BABYLON.Vector3.Zero();
 				this.Position = BABYLON.Vector3.Zero();
 			}
@@ -68,6 +69,24 @@ define(['babylon'], function (BABYLON) {
 				this.backbufferdata[index + 2] = color.b * 255;
 				this.backbufferdata[index + 3] = color.a * 255;
 			};
+			Device.prototype.drawBline = function (point0, point1) {
+				var x0 = point0.x >> 0;
+				var y0 = point0.y >> 0;
+				var x1 = point1.x >> 0;
+				var y1 = point1.y >> 0;
+				var dx = Math.abs(x1 - x0);
+				var dy = Math.abs(y1 - y0);
+				var sx = (x0 < x1) ? 1 : -1;
+				var sy = (y0 < y1) ? 1 : -1;
+				var err = dx - dy;
+				while(true) {
+					this.drawPoint(new BABYLON.Vector2(x0, y0));
+					if((x0 == x1) && (y0 == y1)) break;
+					var e2 = 2 * err;
+					if(e2 > -dy) { err -= dy; x0 += sx; }
+					if(e2 < dx) { err += dx; y0 += sy; }
+				}
+			};
 
 			// Project takes some 3D coordinates and transform them
 			// in 2D coordinates using the transformation matrix
@@ -110,12 +129,26 @@ define(['babylon'], function (BABYLON) {
 
 					var transformMatrix = worldMatrix.multiply(viewMatrix).multiply(projectionMatrix);
 
+					//draw point
 					for (var indexVertices = 0; indexVertices < cMesh.Vertices.length; indexVertices++) {
 						// First, we project the 3D coordinates into the 2D space
 						var projectedPoint = this.project(cMesh.Vertices[indexVertices], transformMatrix);
 						// Then we can draw on screen
 						this.drawPoint(projectedPoint);
 					}
+
+					//draw line
+					for (var indexFaces = 0; indexFaces < cMesh.Faces.length; indexFaces++) {
+						// First, we project the 3D coordinates into the 2D space
+						var p1 = this.project(cMesh.Vertices[cMesh.Faces[indexFaces].A], transformMatrix)
+						var p2 = this.project(cMesh.Vertices[cMesh.Faces[indexFaces].B], transformMatrix)
+						var p3 = this.project(cMesh.Vertices[cMesh.Faces[indexFaces].C], transformMatrix)
+						// Then we can draw on screen
+						this.drawBline(p1, p2);
+						this.drawBline(p2, p3);
+						this.drawBline(p3, p1);
+					}
+
 				}
 			};
 			return Device;
@@ -127,26 +160,39 @@ define(['babylon'], function (BABYLON) {
 	var device;
 	var mesh;
 	var meshes = [];
-	var mera;
+	var camera;
 
 	function init() {
 		canvas = document.getElementById("main_canvas");
-		mesh = new SoftEngine.Mesh("Cube", 8);
+		mesh = new SoftEngine.Mesh("Cube", 8, 12);
 		meshes.push(mesh);
-		mera = new SoftEngine.Camera();
+		camera = new SoftEngine.Camera();
 		device = new SoftEngine.Device(canvas);
 
 		mesh.Vertices[0] = new BABYLON.Vector3(-1, 1, 1);
 		mesh.Vertices[1] = new BABYLON.Vector3(1, 1, 1);
 		mesh.Vertices[2] = new BABYLON.Vector3(-1, -1, 1);
-		mesh.Vertices[3] = new BABYLON.Vector3(-1, -1, -1);
+		mesh.Vertices[3] = new BABYLON.Vector3(1, -1, 1);
 		mesh.Vertices[4] = new BABYLON.Vector3(-1, 1, -1);
 		mesh.Vertices[5] = new BABYLON.Vector3(1, 1, -1);
-		mesh.Vertices[6] = new BABYLON.Vector3(1, -1, 1);
-		mesh.Vertices[7] = new BABYLON.Vector3(1, -1, -1);
+		mesh.Vertices[6] = new BABYLON.Vector3(1, -1, -1);
+		mesh.Vertices[7] = new BABYLON.Vector3(-1, -1, -1);
+		
+		mesh.Faces[0] = { A : 0, B : 1, C : 2 };
+		mesh.Faces[1] = { A : 1, B : 2, C : 3 };
+		mesh.Faces[2] = { A : 1, B : 3, C : 6 };
+		mesh.Faces[3] = { A : 1, B : 5, C : 6 };
+		mesh.Faces[4] = { A : 0, B : 1, C : 4 };
+		mesh.Faces[5] = { A : 1, B : 4, C : 5 };
+		mesh.Faces[6] = { A : 2, B : 3, C : 7 };
+		mesh.Faces[7] = { A : 3, B : 6, C : 7 };
+		mesh.Faces[8] = { A : 0, B : 2, C : 7 };
+		mesh.Faces[9] = { A : 0, B : 4, C : 7 };
+		mesh.Faces[10] = { A : 4, B : 5, C : 6 };
+		mesh.Faces[11] = { A : 4, B : 6, C : 7 };
 
-		mera.Position = new BABYLON.Vector3(0, 0, 10);
-		mera.Target = new BABYLON.Vector3(0, 0, 0);
+		camera.Position = new BABYLON.Vector3(0, 0, 10);
+		camera.Target = new BABYLON.Vector3(0, 0, 0);
 
 		// Calling the HTML5 rendering loop
 		requestAnimationFrame(drawingLoop);
@@ -161,7 +207,7 @@ define(['babylon'], function (BABYLON) {
 		mesh.Rotation.y += 0.01;
 
 		// Doing the various matrix operations
-		device.render(mera, meshes);
+		device.render(camera, meshes);
 		// Flushing the back buffer into the front buffer
 		device.present();
 
